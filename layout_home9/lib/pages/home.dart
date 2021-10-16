8b39d0bd0384f62158054c98159de8b19105afeb
maneekae <92638873+maneekae@users.noreply.github.com>
2021-10-16 23:25:41 +0700

    อัปโหลดโปรเจค flutter / layout_home9 ครั้งแรก

---
+++ layout_home9/lib/pages/home.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:layout/pages/detail.dart';
+
+class HomePage extends StatefulWidget {
+  //const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ดอกไม้มงคลที่ควรปลูกไว้ในบ้าน"),
+      ),
+      body: Padding(
+          padding: EdgeInsets.all(20),
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              var data = json.decode(
+                  snapshot.data.toString()); //[{คอมพิวเตอร์คืออะไร...},{},{}]
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return MyBox(data[index]['title'], data[index]['subtitle'],
+                      data[index]['image_url'], data[index]['detail']);
+                },
+                itemCount: data.length,
+              );
+            },
+            future:
+                DefaultAssetBundle.of(context).loadString('assets/data.json'),
+          )),
+    );
+  }
+
+  Widget MyBox(String title, String subtitle, String image_url, String detail) {
+    var v1, v2, v3, v4;
+    v1 = title;
+    v2 = subtitle;
+    v3 = image_url;
+    v4 = detail;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(20),
+      height: 200,
+      decoration: BoxDecoration(
+          //color: Colors.blue.shade100, //ใส่สีBGในกรอบ
+          borderRadius: BorderRadius.circular(20), //รัศมีขอบโค้ง
+          image: DecorationImage(
+              image: NetworkImage(image_url),
+              fit: BoxFit.cover, //ทำให้รูปมีขนาดเท่ากรอบ
+              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.50),
+                  BlendMode.darken) //ทำให้รูปเข้มขึ้น
+              )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+                fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            subtitle,
+            style: TextStyle(fontSize: 15, color: Colors.white),
+          ),
+          SizedBox(height: 8),
+          TextButton(
+              onPressed: () {
+                print("Next Page >>>>");
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailPage(v1, v2, v3, v4)));
+              },
+              child: Text("อ่านต่อ"))
+        ],
+      ),
+    );
+  }
+}
